e1e9ef10ac8a743bfd0a9dbad8ba38b9082ff523
rexlManu <rexlmanude@gmail.com>
2019-05-10 15:00:06 +0200

    Code reformat

--- program.ino
+++ program.ino
@@ -2,36 +2,42 @@
   Project for the school
 */
 
-int pins[1][7] = {{0,1,2,3,4,5,6}};
+int pins[1][7] = {{0, 1, 2, 3, 4, 5, 6}};
 
 int currentPin = 0;
 int timer = 0;
 
-void setup() {
-   for(int i = 0; i < 6; i++)pinMode(i, OUTPUT);
-     pinMode(7, INPUT);
+void setup()
+{
+  for (int i = 0; i < 6; i++)
+    pinMode(i, OUTPUT);
+  pinMode(7, INPUT);
 
-   currentPin = 0;
-   timer = 0;
+  currentPin = 0;
+  timer = 0;
 }
 
-void loop() {
-     for(int i = 0; i < 6; i++)
-     digitalWrite(pins[currentPin][i], 0);
-    for(int i = 0; i < sizeof(pins[currentPin]); i++)
+void loop()
+{
+  for (int i = 0; i < 6; i++)
+    digitalWrite(pins[currentPin][i], 0);
+  for (int i = 0; i < sizeof(pins[currentPin]); i++)
     digitalWrite(pins[currentPin][i], 1);
-    
-    
-    if(digitalRead(7) == 1){
-      if(timer == 3){
-        currentPin = 5;
-        return;
-      }
-    
-           if(currentPin == sizeof(pins[currentPin])-1)currentPin = 0;
-      else currentPin++; 
-      timer++;
+
+  if (digitalRead(7) == 1)
+  {
+    if (timer == 3)
+    {
+      currentPin = 5;
       return;
     }
-    timer = 0;
+
+    if (currentPin == sizeof(pins[currentPin]) - 1)
+      currentPin = 0;
+    else
+      currentPin++;
+    timer++;
+    return;
+  }
+  timer = 0;
 }
